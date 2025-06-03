430129b7cb74c2a04eb3791b52dfd0c2f346bbec
Sir Yoga <121437273+sergisanodze@users.noreply.github.com>
2025-06-04 00:01:59 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//ორი წინასწარ აღებული რიცხვის ჯამი და სხვაობა
+
+// პირველი რიცხვის არჩევა
+Console.WriteLine("Enter a first whole number!");
+int firstNumber;
+firstNumber = int.Parse(Console.ReadLine());
+// მეორე რიცხვის არჩევა
+Console.WriteLine("Enter a second whole number!");
+int secondNumber;
+secondNumber = int.Parse(Console.ReadLine());
+// მიმატება პირველი და მეორე რიცხვების
+int summary;
+summary = firstNumber + secondNumber;
+//გამოკლება 
+int sub;
+sub = firstNumber - secondNumber;
+// ნამრავლი
+int multiply;
+multiply = firstNumber * secondNumber;
+// გაყოფა
+int division;
+division = firstNumber / secondNumber;
+// ნაშთი
+int modulus;
+modulus = firstNumber % secondNumber;
+//საბოლოო პასუხი
+Console.WriteLine("Your summary is: " + summary 
+                        + "\n" +
+                  "Your substraction is: " + sub 
+                        + "\n" +
+                  "Your multiplication is: " + multiply 
+                        + "\n" +
+                  "Your division is: " + division
+                        + "\n" +
+                  "Your modulus is: " + modulus);  
+Console.ReadKey();
+
